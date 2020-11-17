72cdd7bda7f8b055cc3945394a0600937bc795cc
rukshankr <rukshankr@users.noreply.github.com>
2020-11-17 23:15:06 +0530

    report generation

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -3,26 +3,45 @@ import React, { useState } from 'react'
 import PinBox from '../components/PinBox';
 import axios from 'axios';
 
-const sendGetRequest = () => {
-
-    return axios({
-        url: 'http://localhost:3000/repair/getRepairs',
-        method: 'get'
-    }).then(response => {
-
-        console.log(response.data.data);
-        return response.data;
-    })
-};
+interface RepairPut {
+    put: boolean;
+    message?: string;
+}
 
 const Reports: React.FC = () => {
-    const [repairs, setRepairs] = useState([]);
+    const [reports, setReports] = useState([]);
     const [employeeID, setEmployeeID] = useState<string>();
-    const [date, setDate] = useState<string>();
+    const [initialDate, setinitialDate] = useState<string>();
+    const [finalDate, setfinalDate] = useState<string>();
+    const [putRepair, setPutRepair] = useState<RepairPut>({ put: false });
+    const [emID, setEmID] = useState<string>();
+    const [division, setDivision] = useState<string>();
+    const [subdivision, setSubdivision] = useState<string>();
 
-    React.useEffect(() => {
-        sendGetRequest().then(data => setRepairs(data.data));
-    }, []);
+    const searchJSON = {
+        inspectorID: employeeID,
+        initialDate: initialDate,
+        finalDate: finalDate
+    };
+
+    const searchReports: any = (repairJSON: string) => {
+        console.log(repairJSON)
+        return axios.post(`http://localhost:3000/report/getReports`, repairJSON).then(response => {
+            console.log(response.data.data)
+            if (response.data !== "") {
+                setPutRepair({ put: true, message: response.data.message })
+                setReports(response.data.data)
+                setEmID(response.data.data[0].Name)
+                setDivision(response.data.data[0].Division)
+                setSubdivision(response.data.data[0].SubDivision)
+            }
+            else setPutRepair({ put: true, message: "Results not found" })
+        })
+            .catch(error => {
+                setPutRepair({ put: true, message: error })
+            })
+    };
+
 
     return (
         <IonPage>
@@ -49,7 +68,7 @@ const Reports: React.FC = () => {
                                     <IonRow>
                                         <IonCol>
                                             <IonItem>
-                                                <IonLabel position="floating">Employee ID:</IonLabel>
+                                                <IonLabel position="floating">Employee ID: </IonLabel>
                                                 <IonInput value={employeeID} type="search" onIonChange={(e) => setEmployeeID(e.detail.value!)} />
                                             </IonItem>
                                         </IonCol>
@@ -57,13 +76,21 @@ const Reports: React.FC = () => {
                                     <IonRow>
                                         <IonCol>
                                             <IonItem>
-                                                <IonInput value={date} type="date" onIonChange={(e) => setDate(e.detail.value!)}>Date: </IonInput>
+                                                <IonInput value={initialDate} type="date" onIonChange={(e) => setinitialDate(e.detail.value!)}>From: </IonInput>
+                                            </IonItem>
+                                        </IonCol>
+                                        <IonCol>
+                                            <IonItem>
+                                                <IonInput value={finalDate} type="date" onIonChange={(e) => setfinalDate(e.detail.value!)}>To: </IonInput>
                                             </IonItem>
                                         </IonCol>
                                     </IonRow>
                                     <IonRow>
                                         <IonCol>
-                                            <IonButton color="danger" /*onClick={() => }*/>Search</IonButton>
+                                            <IonButton
+                                                color="danger"
+                                                onClick={() => searchReports(searchJSON)}>
+                                                Search</IonButton>
                                         </IonCol>
                                     </IonRow>
                                 </IonCardContent>
@@ -80,19 +107,25 @@ const Reports: React.FC = () => {
 
                                     <IonRow>
                                         <IonCol>
-                                            <IonLabel position="fixed">Test Date:</IonLabel><br /><br />
+                                            <IonLabel
+                                                position="fixed">
+                                                Employee's Name: {emID ? emID : ""}</IonLabel><br /><br />
 
 
                                         </IonCol>
                                     </IonRow>
                                     <IonRow>
                                         <IonCol>
-                                            <IonLabel position="fixed">Employee's Name:</IonLabel><br /><br />
+                                            <IonLabel
+                                                position="fixed">
+                                                Divsion: {division ? division : ""}</IonLabel><br /><br />
                                         </IonCol>
                                     </IonRow>
                                     <IonRow>
                                         <IonCol>
-                                            <IonLabel position="fixed">Inspection Remarks:</IonLabel><br /><br />
+                                            <IonLabel 
+                                            position="fixed">
+                                                Subdivison:{subdivision ? subdivision : ""}</IonLabel><br /><br />
 
                                         </IonCol>
                                     </IonRow>
@@ -115,12 +148,12 @@ const Reports: React.FC = () => {
                                 </IonCardHeader>
                                 <IonCardContent>
                                     {
-                                        repairs.map(item => {
+                                        reports.map(item => {
                                             return (
                                                 <IonRow className="ion-justify-content-center">
-                                                    <IonCol size="4">{item['EngineerID']}</IonCol>
-                                                    <IonCol size="4">{item['RepairDate']}</IonCol>
-                                                    <IonCol size="4">{item['Comment']}</IonCol>
+                                                    <IonCol size="4">{item['testID']}</IonCol>
+                                                    <IonCol size="4">{item['Division']}</IonCol>
+                                                    <IonCol size="4">{item['result']}</IonCol>
                                                 </IonRow>
                                             )
                                         })
