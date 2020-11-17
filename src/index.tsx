00926c68a65589cbaacf96e6898a1647dcb23294
yomalp <yomalp777@gmail.com>
2020-11-17 19:26:09 +0530

    Fixed misc

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-<<<<<<< HEAD
-import { createStore } from 'redux';
-// import allReducers from './reducers';
-
-// const store = createStore(
-//     allReducers
-// );
-=======
-
->>>>>>> e3c6b83a563f31ac7124e1782055c89a16264e11
 
 const domain: string = process.env.REACT_APP_AUTH0_DOMAIN!;
 const clientId: string = process.env.REACT_APP_AUTH0_CLIENT_ID!;
